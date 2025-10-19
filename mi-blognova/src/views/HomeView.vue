bf78206a3f57858d5dafd094f89681359983310b
GMort01 <manuel.morales01@unicatolica.edu.co>
2025-10-18 19:49:59 -0500

    actualizacion

--- mi-blognova/src/views/HomeView.vue
+++ mi-blognova/src/views/HomeView.vue
@@ -11,7 +11,7 @@ import { reactive } from 'vue'
 
 
 const posts = reactive([
-{ id:1, title:'Zona Cero: Gritos Silenciosos', excerpt:'Mundo post-apocalíptico', image:'/src/assets/portada-2.png', comments:[{author:'Lector Anónimo', text:'¡Increíble historia!'}]},
+{ id:1, title:'Zona Cero: Gritos Silenciosos', excerpt:'Mundo post-apocalíptico', image:'/src/assets/portada 2.png', comments:[{author:'Lector Anónimo', text:'¡Increíble historia!'}]},
 { id:2, title:'El Mundo De Hilda', excerpt:'Un adelanto con un toque personal...', image:'/src/assets/Hilda.jpg', comments:[{author:'Sofía', text:'Siempre me ha gustado el universo de Hilda.'}]},
 { id:3, title:'Explorando la creatividad', excerpt:'Consejos y trucos para inspirarte...', image:'/src/assets/Knight-Dungeon.jpg', comments:[]}
 ])
